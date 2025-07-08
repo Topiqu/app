0fd91f1771e7e9193123223e89d188ed84f258f1
BigJohnn1 <vojtaben15@seznam.cz>
2025-07-08 11:35:44 +0200

    added app seo meta

--- app.vue
+++ app.vue
@@ -5,3 +5,12 @@
     <NuxtLayout> <NuxtPage /> </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'RASG blog',
+  description: 'Vítejte v RASG blogu.',
+  keywords: 'blog, RASG, insights, stories, journey',
+  author: 'Big John',
+})
+</script>
