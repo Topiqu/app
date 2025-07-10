88098ddc4d470a701dfab8747ad4b556b0dbd8f1
BigJohnn1 <vojtaben15@seznam.cz>
2025-07-10 07:14:38 +0200

    tags endpoints

--- server/api/tags/index.post.ts
+++ server/api/tags/index.post.ts
@@ -1,3 +1,28 @@
-// export default defineEventHandler(async (event) => {
-//   return 'Hello Nitro'
-// })
+import { createError } from 'h3'
+import { getServerSession } from '#auth'
+
+export default defineEventHandler(async (event) => {
+  const user = (await getServerSession(event))?.user
+  if (!user)
+    throw createError({ statusCode: 401, statusMessage: 'Neautorizováno' })
+
+  const body = await readValidatedBody(event, TagCreateSchema.parse)
+
+  const existingTag = await prisma.tag.findUnique({
+    where: { name: body.name },
+  })
+
+  if (existingTag)
+    throw createError({
+      statusCode: 409,
+      statusMessage: 'Tag s tímto názvem už existuje',
+    })
+
+  const tag = await prisma.tag.create({
+    data: {
+      name: body.name,
+    },
+  })
+
+  return tag
+})
