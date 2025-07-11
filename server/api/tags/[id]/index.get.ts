48fe471d180fb6ceb03f71cfe1570a8c497aa897
BigJohnn1 <vojtaben15@seznam.cz>
2025-07-11 09:15:11 +0200

    implement tag retrieval endpoint

---
+++ server/api/tags/[id]/index.get.ts
@@ -0,0 +1,36 @@
+import { getServerSession } from '#auth'
+
+export default defineEventHandler(async (event) => {
+  const user = (await getServerSession(event))?.user
+  if (!user)
+    throw createError({ statusCode: 401, statusMessage: 'Neautorizováno' })
+
+  const id = getRouterParam(event, 'id')
+  if (!id)
+    throw createError({ statusCode: 400, statusMessage: 'ID je povinné' })
+
+  const tag = await prisma.tag.findUnique({
+    where: { id },
+    include: {
+      articles: {
+        include: {
+          article: {
+            include: {
+              tags: {
+                include: {
+                  tag: true,
+                },
+              },
+              user: true,
+            },
+          },
+        },
+      },
+    },
+  })
+
+  if (!tag)
+    throw createError({ statusCode: 404, statusMessage: 'Tag nenalezen' })
+
+  return tag
+})
