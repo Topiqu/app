817b5e90009d1c22b04fab713ff84583e99e5318
BigJohnn1 <vojtaben15@seznam.cz>
2025-07-14 08:46:48 +0200

    reading time centralised in singleton file -- for now

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,13 +1,39 @@
 import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => new PrismaClient()
+const prismaClientSingleton = () =>
+  new PrismaClient().$extends({
+    query: {
+      article: {
+        create({ args, query }) {
+          if (typeof args.data.content === 'string') {
+            const words = args.data.content.trim().split(/\s+/).length
+            args.data.readingTime = Math.ceil(words / 200)
+          }
+          return query(args)
+        },
+        update({ args, query }) {
+          const content = args.data.content
+          const value =
+            typeof content === 'object' && 'set' in content
+              ? content.set
+              : content
+
+          if (typeof value === 'string') {
+            const words = value.trim().split(/\s+/).length
+            args.data.readingTime = Math.ceil(words / 200)
+          }
+
+          return query(args)
+        },
+      },
+    },
+  })
 
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>
 } & typeof global
 
-const prisma = globalThis.prismaGlobal || prismaClientSingleton()
-
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 globalThis.prismaGlobal = prisma
 
 export default prisma
