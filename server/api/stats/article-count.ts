87a4b496f8c2c52fbc2ad19841052cb562b3a8cf
BigJohnn1 <vojtaben15@seznam.cz>
2025-07-17 15:31:12 +0200

    article count

---
+++ server/api/stats/article-count.ts
@@ -0,0 +1,13 @@
+import { getServerSession } from '#auth'
+
+export default defineEventHandler(async (event) => {
+  const user = (await getServerSession(event))?.user
+  if (!user)
+    throw createError({ statusCode: 401, statusMessage: 'Neautorizováno' })
+
+  const articleCount = await prisma.article.count({
+    where: { userId: user.id },
+  })
+
+  return { articleCount }
+})
