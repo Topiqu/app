cf66524ecbd9a9e1d59bcbc0027a12fac6ec103e
BigJohnn1 <vojtaben15@seznam.cz>
2025-07-17 15:31:23 +0200

    top tags

---
+++ server/api/stats/top-tags.ts
@@ -0,0 +1,28 @@
+import { getServerSession } from '#auth'
+
+export default defineEventHandler(async (event) => {
+  const user = (await getServerSession(event))?.user
+  if (!user)
+    throw createError({ statusCode: 401, statusMessage: 'Neautorizováno' })
+
+  const { take, skip } = await getPagination(event)
+
+  const tags = await prisma.tag.findMany({
+    where: { articles: { some: { article: { userId: user.id } } } },
+    select: {
+      id: true,
+      name: true,
+      articles: { select: { article: { select: { views: true } } } },
+    },
+    skip,
+    take,
+    orderBy: { articles: { _count: 'desc' } },
+  })
+
+  return tags.map((tag) => ({
+    id: tag.id,
+    name: tag.name,
+    views: tag.articles.reduce((sum, a) => sum + a.article.views, 0),
+    articleCount: tag.articles.length,
+  }))
+})
