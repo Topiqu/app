8638c445bfccfc43e5629bf8406e2d0fc611d109
BigJohnn1 <vojtaben15@seznam.cz>
2025-07-30 17:06:18 +0200

    removed @omit from deletedat

--- server/api/users/index.get.ts
+++ server/api/users/index.get.ts
@@ -2,14 +2,8 @@ export default defineEventHandler(async (event) => {
   const user = (await getServerSession(event))?.user
   if (!user || user.role !== 'superadmin') throw createError({ statusCode: 403, message: 'Neautorizováno' })
 
-  return prisma.user.findMany({
+  const db = await getEnhancedPrisma(user)
+  return db.user.findMany({
     where: { role: { not: 'superadmin' } },
-    select: {
-      id: true,
-      username: true,
-      email: true,
-      role: true,
-      deletedAt: true,
-    },
   })
 })
