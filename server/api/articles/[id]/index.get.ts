625e52a8edba25251042fd34daaf1ea745570aa0
BigJohnn1 <vojtaben15@seznam.cz>
2025-08-01 15:46:26 +0200

    changed get to return like count

--- server/api/articles/[id]/index.get.ts
+++ server/api/articles/[id]/index.get.ts
@@ -2,13 +2,22 @@ export default defineEventHandler(async (event) => {
   const user = (await getServerSession(event))?.user
   const slug = getRouterParam(event, 'id')
   if (!slug) throw createError({ statusCode: 400, message: 'Slug je povinný' })
+
+  const sessionId = getCookie(event, 'anon_session')
   const db = await getEnhancedPrisma(user)
+
   const article = await db.article.findUnique({
     where: { slug },
     include: {
       user: { select: { username: true, id: true } },
       tags: { include: { tag: true } },
-      _count: { select: { comments: { where: { deletedAt: null } } } },
+      reactions: true,
+      _count: {
+        select: {
+          comments: { where: { deletedAt: null } },
+          reactions: true,
+        },
+      },
     },
   })
 
@@ -20,5 +29,11 @@ export default defineEventHandler(async (event) => {
   return {
     ...article,
     commentCount: article._count.comments,
+    likes: article._count.reactions,
+    likedByUser: user?.id
+      ? article.reactions.some((r) => r.userId === user.id)
+      : sessionId
+        ? article.reactions.some((r) => r.sessionId === sessionId)
+        : false,
   }
 })
