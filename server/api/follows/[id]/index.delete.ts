965aac8868fab20f96672cc66fc05d35719d98d2
BigJohnn1 <vojtaben15@seznam.cz>
2025-08-07 17:59:07 +0200

    cancel follow

---
+++ server/api/follows/[id]/index.delete.ts
@@ -0,0 +1,18 @@
+export default defineEventHandler(async (event) => {
+  const user = (await getServerSession(event))?.user
+  if (!user) throw createError({ statusCode: 401, message: 'Neautorizováno' })
+  const id = getRouterParam(event, 'id')
+  const db = await getEnhancedPrisma(user)
+
+  const follow = await db.follow.findFirst({
+    where: { followerId: user.id, followedId: id },
+  })
+
+  if (!follow) throw createError({ statusCode: 404, message: 'Follow vztah nenalezen' })
+
+  await db.follow.delete({
+    where: { followerId_followedId: { followerId: user.id, followedId: id! } },
+  })
+
+  return { success: true }
+})
