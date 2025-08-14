43ab020a0bdbeb70f8b5d7a65b2241e3fe59efa4
BigJohnn1 <vojtaben15@seznam.cz>
2025-08-14 09:54:04 +0200

    controlling whether user is banned

--- server/api/comments/index.post.ts
+++ server/api/comments/index.post.ts
@@ -7,6 +7,21 @@ export default defineEventHandler(async (event) => {
 
   const article = await prisma.article.findUnique({
     where: { id: body.articleId },
+    select: { clientSiteId: true },
+  })
+  if (!article) throw createError({ statusCode: 404, message: 'Článek nenalezen' })
+
+  const activeBan = await prisma.userBan.findFirst({
+    where: {
+      userId: user.id,
+      clientSiteId: article.clientSiteId,
+      OR: [{ expiresAt: null }, { expiresAt: { gt: new Date() } }],
+    },
+  })
+  if (activeBan) throw createError({ statusCode: 403, message: 'Nemůžete přidat komentář, jste zabanován' })
+
+  const fullArticle = await prisma.article.findUnique({
+    where: { id: body.articleId },
     select: {
       id: true,
       userId: true,
@@ -16,10 +31,10 @@ export default defineEventHandler(async (event) => {
       user: { select: { email: true, allowNotifs: true } },
     },
   })
-  if (!article) throw createError({ statusCode: 404, message: 'Článek nenalezen' })
+  if (!fullArticle) throw createError({ statusCode: 404, message: 'Článek nenalezen' })
 
   let content = body.content
-  const url = (id: string) => `http://localhost:3000/clanky/${article.slug}#comment-${id}`
+  const url = (id: string) => `http://localhost:3000/clanky/${fullArticle.slug}#comment-${id}`
 
   if (body.parentId) {
     const parent = await prisma.comment.findUnique({
@@ -43,7 +58,7 @@ export default defineEventHandler(async (event) => {
       })
     }
   }
-  if (article.allowedComments) {
+  if (fullArticle.allowedComments) {
     const comment = await prisma.comment.create({
       data: { content, articleId: body.articleId, userId: user.id, parentId: body.parentId || null },
       select: {
@@ -55,11 +70,11 @@ export default defineEventHandler(async (event) => {
       },
     })
 
-    if (article.userId !== user.id && article.user.allowNotifs) {
+    if (fullArticle.userId !== user.id && fullArticle.user.allowNotifs) {
       await useNodeMailer().sendMail({
         from: useRuntimeConfig().from,
-        to: article.user.email,
-        subject: `Nový komentář – ${article.title}`,
+        to: fullArticle.user.email,
+        subject: `Nový komentář – ${fullArticle.title}`,
         text: `Ahoj, ${user.name} okomentoval tvůj článek: "${body.content.slice(0, 50)}...".\nPodívej se zde: ${url(comment.id)}`,
         html: `
       <p>Ahoj,</p>
@@ -71,8 +86,8 @@ export default defineEventHandler(async (event) => {
       await prisma.notification.create({
         data: {
           message: `${user.name} okomentoval váš článek.`,
-          userId: article.userId,
-          articleId: article.id,
+          userId: fullArticle.userId,
+          articleId: fullArticle.id,
           type: 'COMMENT',
         },
       })
