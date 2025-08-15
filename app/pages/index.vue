bfbe0965b4fa7815fb6f07fc6ab0fb5c8306016f
germondai <bruno.krocek@gmail.com>
2025-08-15 20:20:04 +0200

    handle redirection from index page when authenticated based on user role

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,13 @@
 <script lang="ts" setup>
 const { data: auth } = useAuth()
+
+if (auth.value?.user.role === 'superadmin') await navigateTo('/master')
+else if (auth.value?.user.role === 'admin') await navigateTo('/admin')
+else if (auth.value?.user.role === 'reader') await navigateTo('/uzivatel')
 </script>
 
 <template>
   <main class="flex items-center justify-center">
-    <UserAccount v-if="auth" />
-    <AuthForm v-else />
+    <AuthForm />
   </main>
 </template>
