17aa384c48cbd89dfe37081dcfed5f77eedf764f
germondai <bruno.krocek@gmail.com>
2025-08-15 17:03:35 +0200

    updated Modal component

--- app/components/Modal/index.vue
+++ app/components/Modal/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <slot>
+  <slot v-bind="actions">
     <button type="button" @click="open = true">Open dialog</button>
   </slot>
   <TransitionRoot appear :show="open" as="template">
-    <Dialog as="div" class="relative z-[1000]" @close="open = false">
+    <Dialog as="div" class="relative z-[1000]" @close="close">
       <TransitionChild
         as="template"
         enter="ease-out duration-300"
@@ -30,9 +30,9 @@
             class="h-11/12 w-full max-w-xl bg-white/80 backdrop-blur-xl rounded-3xl shadow-2xl p-8 flex flex-col gap-6 overflow-y-auto border border-gray-200/70"
           >
             <div class="flex justify-between items-center gap-4">
-              <slot name="header">
+              <slot name="header" v-bind="actions">
                 <div class="grow flex flex-col">
-                  <slot name="title">
+                  <slot name="title" v-bind="actions">
                     <DialogTitle
                       v-if="title"
                       class="text-2xl font-bold bg-gradient-to-r from-gray-600 to-gray-400 bg-clip-text text-transparent"
@@ -40,7 +40,7 @@
                       {{ title }}
                     </DialogTitle>
                   </slot>
-                  <slot name="description">
+                  <slot name="description" v-bind="actions">
                     <DialogDescription
                       v-if="description"
                       class="text-lg font-semibold bg-gradient-to-r from-gray-600 to-gray-400 bg-clip-text text-transparent"
@@ -49,10 +49,10 @@
                     </DialogDescription>
                   </slot>
                 </div>
-                <slot v-if="close" name="close">
+                <slot v-if="closeBtn" name="close" v-bind="actions">
                   <button
                     class="ml-auto flex items-center justify-center p-2 rounded-full hover:bg-gray-100 transition-colors"
-                    @click="open = false"
+                    @click="close"
                   >
                     <Icon name="mdi:close" class="size-5" />
                   </button>
@@ -61,11 +61,11 @@
             </div>
 
             <div class="grow">
-              <slot name="content" />
+              <slot name="content" v-bind="actions" />
             </div>
 
             <div class="flex justify-end">
-              <slot name="footer" />
+              <slot name="footer" v-bind="actions" />
             </div>
           </DialogPanel>
         </TransitionChild>
@@ -77,7 +77,16 @@
 <script setup lang="ts">
 import { Dialog, DialogPanel, DialogTitle, TransitionRoot, TransitionChild, DialogDescription } from '@headlessui/vue'
 
-const { close = true } = defineProps<{ title?: string; description?: string; close?: boolean }>()
-
 const open = defineModel<boolean>()
+
+const { close: closeBtn = true, onClose } = defineProps<{
+  title?: string
+  description?: string
+  close?: boolean
+  onClose?: () => void
+}>()
+
+const close = onClose || (() => (open.value = false))
+
+const actions = computed(() => ({ open, close }))
 </script>
