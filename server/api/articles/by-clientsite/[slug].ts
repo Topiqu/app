0ad77b1f139466198dd4f54b3c33ea4204165d3b
BigJohnn1 <vojtaben15@seznam.cz>
2025-08-19 20:58:29 +0200

    pagination for api

--- server/api/articles/by-clientsite/[slug].ts
+++ server/api/articles/by-clientsite/[slug].ts
@@ -1,39 +1,28 @@
 export default defineEventHandler(async (event) => {
+  const user = (await getServerSession(event))?.user
   const slug = getRouterParam(event, 'slug')
   if (!slug) throw createError({ statusCode: 400, message: 'Neplatný požadavek' })
+  const { take, skip } = await getPagination(event)
 
-  const clientSite = await prisma.clientSite.findFirst({
+  const db = await getEnhancedPrisma(user)
+  const clientSite = await db.clientSite.findUnique({
     where: { name: slug },
+    select: { id: true },
   })
+
   if (!clientSite) throw createError({ statusCode: 404, message: 'Blog nenalezen' })
 
-  return prisma.article.findMany({
-    where: { clientSiteId: clientSite.id, status: 'published' },
-    select: {
-      id: true,
-      slug: true,
-      title: true,
-      content: true,
-      imageUrl: true,
-      createdAt: true,
-      readingTime: true,
-      user: {
-        select: { id: true, username: true, avatarUrl: true },
-      },
-      tags: {
-        select: {
-          tag: {
-            select: { id: true, name: true, slug: true },
-          },
-        },
-      },
-      _count: {
-        select: {
-          comments: { where: { deletedAt: null } },
-          reactions: true,
-        },
-      },
+  const articles = await db.article.findMany({
+    where: { clientSiteId: clientSite.id },
+    take,
+    skip,
+    include: {
+      tags: { include: { tag: true } },
+      user: { select: { id: true, username: true, email: true, role: true, avatarUrl: true } },
+      _count: { select: { comments: true, reactions: true } },
     },
     orderBy: { createdAt: 'desc' },
   })
+
+  return articles
 })
