a8532e2f104374fd09460ebb46128f5686c9da74
BigJohnn1 <vojtaben15@seznam.cz>
2025-08-26 09:03:24 +0200

    emit delete

--- app/composables/useArticleEvent.ts
+++ app/composables/useArticleEvent.ts
@@ -5,5 +5,7 @@ const emitter = mitt()
 export const useArticleEvent = () => {
   const onArticleCreated = (cb: () => void) => emitter.on('article:created', cb)
   const emitArticleCreated = () => emitter.emit('article:created')
-  return { onArticleCreated, emitArticleCreated }
+  const onArticleDeleted = (cb: () => void) => emitter.on('article:deleted', cb)
+  const emitArticleDeleted = () => emitter.emit('article:deleted')
+  return { onArticleCreated, emitArticleCreated, onArticleDeleted, emitArticleDeleted }
 }
