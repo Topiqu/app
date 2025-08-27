92fa4446c1f8cb4bb80684060accdebfd3970f2a
BigJohnn1 <vojtaben15@seznam.cz>
2025-08-27 09:39:46 +0200

    emit client delete

--- app/composables/useClientEvent.ts
+++ app/composables/useClientEvent.ts
@@ -5,5 +5,7 @@ const emitter = mitt()
 export const useClientEvent = () => {
   const onClientCreated = (cb: () => void) => emitter.on('client:created', cb)
   const emitClientCreated = () => emitter.emit('client:created')
-  return { onClientCreated, emitClientCreated }
+  const onClientDeleted = (cb: () => void) => emitter.on('client:deleted', cb)
+  const emitClientDeleted = () => emitter.emit('client:deleted')
+  return { onClientCreated, emitClientCreated, onClientDeleted, emitClientDeleted }
 }
