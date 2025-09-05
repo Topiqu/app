464c3b53d9a2b4057739fb201dbcbcf151caa014
germondai <bruno.krocek@gmail.com>
2025-09-05 22:49:16 +0200

    recreated /api/clients get endpoint to handle pagination and query filtering

--- server/api/clients/index.get.ts
+++ server/api/clients/index.get.ts
@@ -1,12 +1,45 @@
-export default defineEventHandler(async () => {
-  return await prisma.clientSite.findMany({
-    include: {
-      users: {
-        select: {
-          username: true,
-          email: true,
+export default defineEventHandler(async (event) => {
+  const user = (await getServerSession(event))?.user
+  const { skip, take } = await getPagination(event)
+  const query = getQuery(event).query as string | undefined
+
+  const db = await getEnhancedPrisma(user)
+
+  const [data, total] = await Promise.all([
+    db.clientSite.findMany({
+      include: {
+        users: {
+          select: {
+            username: true,
+            email: true,
+          },
         },
       },
-    },
-  })
+      where: {
+        ...(query && {
+          OR: [
+            { name: { contains: query, mode: 'insensitive' } },
+            { audience: { contains: query, mode: 'insensitive' } },
+            { focus: { contains: query, mode: 'insensitive' } },
+          ],
+        }),
+      },
+      orderBy: { createdAt: 'desc' },
+      skip,
+      take,
+    }),
+    db.clientSite.count({
+      where: {
+        ...(query && {
+          OR: [
+            { name: { contains: query, mode: 'insensitive' } },
+            { audience: { contains: query, mode: 'insensitive' } },
+            { focus: { contains: query, mode: 'insensitive' } },
+          ],
+        }),
+      },
+    }),
+  ])
+
+  return { data, total }
 })
