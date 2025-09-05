067eb2e59f8cf7537d00427d6626330a2a334da1
germondai <bruno.krocek@gmail.com>
2025-09-05 22:02:52 +0200

    done bubble menu buttons

--- app/components/BubbleMenu.vue
+++ app/components/BubbleMenu.vue
@@ -1,81 +1,52 @@
 <template>
   <BubbleMenu :editor="editor" class="p-1 flex items-center gap-2 rounded-lg bg-transparent border border-gray-600">
-    <button
-      type="button"
-      class="p-1 text-gray-800 hover:bg-gray-200 rounded inline-flex items-center justify-center"
+    <Button
+      icon="mdi-format-bold"
       title="Bold"
       :disabled="!editor.can().chain().focus().toggleBold().run()"
-      :class="{ 'bg-gray-200': editor.isActive('bold') }"
+      :active="editor.isActive('bold')"
       @mousedown.stop.prevent="editor.chain().focus().toggleBold().run()"
-    >
-      <Icon name="mdi-format-bold" />
-    </button>
-    <button
-      type="button"
-      class="p-1 text-gray-800 hover:bg-gray-200 rounded inline-flex items-center justify-center"
+    />
+    <Button
+      icon="mdi-format-italic"
       title="Italic"
       :disabled="!editor.can().chain().focus().toggleItalic().run()"
-      :class="{ 'bg-gray-200': editor.isActive('italic') }"
+      :active="editor.isActive('italic')"
       @mousedown.stop.prevent="editor.chain().focus().toggleItalic().run()"
-    >
-      <Icon name="mdi-format-italic" />
-    </button>
-    <button
-      type="button"
-      class="p-1 text-gray-800 hover:bg-gray-200 rounded inline-flex items-center justify-center"
+    />
+    <Button
+      icon="mdi-format-strikethrough-variant"
       title="Strike"
       :disabled="!editor.can().chain().focus().toggleStrike().run()"
-      :class="{ 'bg-gray-200': editor.isActive('strike') }"
+      :active="editor.isActive('strike')"
       @mousedown.stop.prevent="editor.chain().focus().toggleStrike().run()"
-    >
-      <Icon name="mdi-format-strikethrough-variant" />
-    </button>
-    <button
-      type="button"
-      class="p-1 text-gray-800 hover:bg-gray-200 rounded inline-flex items-center justify-center"
+    />
+    <Button
+      icon="mdi-code-tags"
       title="Code"
       :disabled="!editor.can().chain().focus().toggleCode().run()"
-      :class="{ 'bg-gray-200': editor.isActive('code') }"
+      :active="editor.isActive('code')"
       @mousedown.stop.prevent="editor.chain().focus().toggleCode().run()"
-    >
-      <Icon name="mdi-code-tags" />
-    </button>
-    <button
-      type="button"
-      class="p-1 text-gray-800 hover:bg-gray-200 rounded inline-flex items-center justify-center"
+    />
+    <Button
+      icon="mdi-format-underline"
       title="Underline"
       :disabled="!editor.can().chain().focus().toggleUnderline().run()"
-      :class="{ 'bg-gray-200': editor.isActive('underline') }"
+      :active="editor.isActive('underline')"
       @mousedown.stop.prevent="editor.chain().focus().toggleUnderline().run()"
-    >
-      <Icon name="mdi-format-underline" />
-    </button>
-    <button
-      type="button"
-      class="p-1 text-gray-800 hover:bg-gray-200 rounded inline-flex items-center justify-center"
-      title="Link"
-      :class="{ 'bg-gray-200': editor.isActive('link') }"
-      @mousedown.stop.prevent="setLink"
-    >
-      <Icon name="mdi-link" />
-    </button>
-    <button
-      type="button"
-      class="p-1 text-gray-800 hover:bg-gray-200 rounded inline-flex items-center justify-center"
+    />
+    <Button icon="mdi-link" title="Link" :active="editor.isActive('link')" @mousedown.stop.prevent="setLink" />
+    <Button
+      icon="mdi-link-off"
       title="Unlink"
       :disabled="!editor.isActive('link')"
       @mousedown.stop.prevent="editor.chain().focus().unsetLink().run()"
-    >
-      <Icon name="mdi-link-off" />
-    </button>
-    <button
-      type="button"
-      class="p-1 text-gray-800 hover:bg-gray-200 rounded inline-flex items-center justify-center"
+    />
+    <Button
+      icon="mdi-format-clear"
       title="Clear"
       @mousedown.stop.prevent="editor.chain().focus().unsetAllMarks().run()"
-    >
-      <Icon name="mdi-format-clear" />
-    </button>
+    />
   </BubbleMenu>
 </template>
 
