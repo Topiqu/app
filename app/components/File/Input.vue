950490551aab8f03128c4acc58247bff6740a061
germondai <bruno.krocek@gmail.com>
2025-09-05 21:40:42 +0200

    better FileInput component

--- app/components/File/Input.vue
+++ app/components/File/Input.vue
@@ -1,37 +1,16 @@
 <template>
-  <div>
-    <input v-if="show" ref="fileInput" type="file" class="hidden" accept="image/*" @change="handleFileChange" />
-    <Icon
-      name="mdi-image-plus"
-      class="p-1 text-gray-800 rounded inline-flex items-center justify-center"
-      @click="open"
-    />
-  </div>
+  <Button icon="mdi-image-plus" @click="open({ accept: 'image/*' })" />
 </template>
 
 <script lang="ts" setup>
-const props = defineProps<{
-  uploadImage: (e: Event) => Promise<void>
-}>()
+const props = defineProps<{ uploadImage: (files: FileList | null) => Promise<void> }>()
 
-const emit = defineEmits<{
-  (e: 'close'): void
-}>()
+const emit = defineEmits<{ (e: 'close'): void }>()
 
-const show = ref(false)
-const fileInput = ref<HTMLInputElement | null>(null)
+const { open, onChange } = useFileDialog()
 
-const open = () => {
-  if (show.value) return
-  show.value = true
-  fileInput.value?.click()
-}
-
-const handleFileChange = async (e: Event) => {
-  await props.uploadImage(e)
-  show.value = false
+onChange(async (files) => {
+  await props.uploadImage(files)
   emit('close')
-}
-
-defineExpose({ open })
+})
 </script>
