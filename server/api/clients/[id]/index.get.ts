1080260564a4628f986de22de598422a55f13227
BigJohnn1 <vojtaben15@seznam.cz>
2025-09-05 12:56:04 +0200

    get api now returns ai user

--- server/api/clients/[id]/index.get.ts
+++ server/api/clients/[id]/index.get.ts
@@ -1,7 +1,37 @@
 export default defineEventHandler(async (event) => {
+  const user = (await getServerSession(event))?.user
+  if (!user || (user.role !== 'superadmin' && user.role !== 'admin')) {
+    throw createError({ statusCode: 401, message: 'Neautorizováno' })
+  }
+
   const id = getRouterParam(event, 'id')
-  return await prisma.clientSite.findUnique({
+  if (!id) {
+    throw createError({ statusCode: 400, message: 'ID nenalezeno' })
+  }
+
+  const clientSite = await prisma.clientSite.findUnique({
     where: { id },
-    include: { users: true, socials: true },
+    include: {
+      users: {
+        where: { role: 'ai' },
+        select: { id: true, username: true, bio: true, avatarUrl: true },
+      },
+      socials: true,
+    },
   })
+
+  if (!clientSite) {
+    throw createError({ statusCode: 404, message: 'Klient nenalezen' })
+  }
+
+  return {
+    ...clientSite,
+    aiUser: clientSite.users[0]
+      ? {
+          username: clientSite.users[0].username,
+          bio: clientSite.users[0].bio,
+          avatarUrl: clientSite.users[0].avatarUrl,
+        }
+      : null,
+  }
 })
