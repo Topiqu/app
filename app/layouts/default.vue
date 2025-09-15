fc2b39a71285235c8c7cfa14ba5f00a90a1cc203
germondai <bruno.krocek@gmail.com>
2025-09-15 22:40:58 +0200

    replaced default layout header-like code with code splitted solid header component

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,16 +1,7 @@
 <template>
-  <div class="min-h-screen max-w-screen flex-1 flex flex-col bg-gray-100 relative">
-    <Sidebar v-if="auth && isAdmin" :isOpen="sidebarOpen" @update:isOpen="sidebarOpen = $event || false" />
-    <div class="md:hidden p-4 bg-white shadow flex items-center justify-between">
-      <button v-if="auth && isAdmin" @click="sidebarOpen = !sidebarOpen">
-        <Icon name="mdi:menu" class="w-6 h-6 text-black" />
-      </button>
-    </div>
-    <div class="fixed top-2 right-2 flex items-center justify-between gap-2 z-100">
-      <UserAccount />
-      <ThemeToggle />
-      <NotificationBar />
-    </div>
+  <div class="pt-4 min-h-screen max-w-screen flex-1 flex flex-col bg-gray-100 relative">
+    <Header v-model:isSidebarOpen="isSidebarOpen" />
+    <Sidebar v-if="auth && isAdmin" v-model:isOpen="isSidebarOpen" />
     <slot />
     <ButtonBackToTop />
     <ClientVersion v-if="isAdmin" :userId="auth?.user.id!" />
@@ -20,7 +11,7 @@
 <script setup lang="ts">
 const { data: auth } = useAuth()
 
-const sidebarOpen = shallowRef<boolean>(true)
+const isSidebarOpen = shallowRef<boolean>(true)
 
 const isAdmin = computed(() => ['admin', 'superadmin'].includes(auth?.value?.user.role || ''))
 </script>
