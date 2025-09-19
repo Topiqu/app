c048f5ec7d41c36c1a084c5d974f8b65ce642286
BigJohnn1 <vojtaben15@seznam.cz>
2025-09-19 18:10:33 +0200

    global cs middleware

---
+++ app/middleware/session.global.ts
@@ -0,0 +1,16 @@
+export default defineNuxtRouteMiddleware(async () => {
+  const { data: auth, signOut } = useAuth()
+
+  if (import.meta.server) return
+  if (auth.value?.user && auth.value?.user.sessionId)
+    try {
+      await $fetch('/api/sessions/ping', { method: 'POST' })
+    } catch (e: any) {
+      console.log('cau mafe' + e.data.message)
+      await signOut()
+      throw createError({
+        statusCode: 401,
+        message: 'Relace byla zrušena, přihlaste se znovu',
+      })
+    }
+})
