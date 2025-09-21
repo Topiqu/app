1653fdee5abdedd7a74583377300c3a634d0c457
BigJohnn1 <vojtaben15@seznam.cz>
2025-09-21 11:50:57 +0200

    dynamic loading bar color based on clientsite theme

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,12 @@
 <template>
-  <NuxtLoadingIndicator class="z-[9999]" />
+  <NuxtLoadingIndicator
+    class="z-[9999]"
+    :color="
+      clientSite?.theme && Object.keys(themeColors).includes(clientSite.theme)
+        ? themeColors[clientSite.theme]
+        : themeColors.blue
+    "
+  />
   <StatusBar />
   <div>
     <NuxtLayout> <NuxtPage /> </NuxtLayout>
@@ -7,6 +14,30 @@
 </template>
 
 <script setup lang="ts">
+import type { themes } from '~/composables/theme'
+
+const slug = 'GameDev'
+
+const { data: clientSite } = await useFetch(`/api/clients/slug/${slug}`, { server: false })
+
+const themeColors: Record<keyof typeof themes, string> = {
+  blue: '#2563eb',
+  green: '#16a34a',
+  red: '#dc2626',
+  purple: '#7c3aed',
+  orange: '#f97316',
+  teal: '#0d9488',
+  yellow: '#eab308',
+  pink: '#ec4899',
+  indigo: '#4f46e5',
+  gray: '#6b7280',
+  lime: '#65a30d',
+  sky: '#0ea5e9',
+  amber: '#f59e0b',
+  cyan: '#06b6d4',
+  violet: '#8b5cf6',
+}
+
 useSeoMeta({
   title: 'Topiqu AI blog',
   description: 'Vítejte v Topiqu blogu.',
