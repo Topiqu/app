c51216ef96416823d8263537a47b2cb797a06244
BigJohnn1 <vojtaben15@seznam.cz>
2025-09-21 17:53:03 +0200

    expanded error.vue

--- app/error.vue
+++ app/error.vue
@@ -1,6 +1,14 @@
 <template>
   <NuxtLayout name="default">
-    <Status type="error" :title="title" :message="message" :actionText="$t('common.backHome')" actionTo="/" />
+    <Status
+      type="error"
+      :title="title"
+      :message="message"
+      :actionText="$t('common.actions.home')"
+      :actionTo="'/'"
+      :stackTrace="stackTrace"
+      :errorCode="statusCode"
+    />
   </NuxtLayout>
 </template>
 
@@ -12,15 +20,32 @@ const statusCode = computed(() => error.value?.statusCode)
 const title = computed(() => {
   switch (statusCode.value) {
     case 404:
-      return 'Stránka nenalezena'
+      return $t('common.errTypes.notFound')
+    case 401:
+      return $t('common.errTypes.unauthorized')
+    case 403:
+      return $t('common.errTypes.forbidden')
+    case 400:
+      return $t('common.errTypes.badRequest')
     case 429:
-      return 'Příliš mnoho požadavků'
+      return $t('common.errTypes.tooManyRequests')
     case 500:
-      return 'Chyba serveru'
+      return $t('common.errTypes.server')
+    case 503:
+      return $t('common.errTypes.serviceUnavailable')
+    case 409:
+      return $t('common.errTypes.conflict')
+    case 406:
+      return $t('common.errTypes.notAcceptable')
+    case 504:
+      return $t('common.errTypes.gatewayTimeout')
+    case 422:
+      return $t('common.errTypes.unprocessableEntity')
     default:
-      return 'Nastala neočekávaná chyba'
+      return $t('common.errTypes.internalError')
   }
 })
 
 const message = computed(() => error.value?.message || '')
+const stackTrace = computed(() => error.value?.stack || '')
 </script>
