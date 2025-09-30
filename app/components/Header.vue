5a564e39e0f342a6108bb34a7ef80f0d4105a492
germondai <bruno.krocek@gmail.com>
2025-09-30 10:06:33 +0200

    fixed header pointer events

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed top-0 left-0 p-2 h-14 w-full flex items-center justify-between max-md:bg-gray-100/60 max-md:backdrop-blur-sm !dark:bg-transparent z-[999]"
+    class="fixed top-0 left-0 p-2 h-16 w-full flex items-center justify-between max-md:bg-gray-100/60 max-md:backdrop-blur-sm !dark:bg-transparent pointer-events-none [&>*]:pointer-events-auto z-[999]"
   >
     <Button
       v-if="auth && isAdmin"
@@ -10,7 +10,7 @@
       @click="isSidebarOpen = !isSidebarOpen"
     />
     <NuxtLink to="/" class="flex items-center justify-center gap-2">
-      <NuxtImg :src="clientSite?.logoUrl || '/app-logo.png'" alt="Logo" class="h-20 mt-10" />
+      <NuxtImg :src="clientSite?.logoUrl || '/app-logo.png'" alt="Logo" class="w-14 object-conatin" />
     </NuxtLink>
     <div class="flex items-center justify-between gap-2">
       <UserAccount />
