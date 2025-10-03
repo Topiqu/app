6df363e2841f63d1beeff593d63f9021aef2f480
Big John <vojtaben15@seznam.cz>
2025-10-03 16:32:17 +0200

    server side hostname get

--- app/composables/useClientsite.ts
+++ app/composables/useClientsite.ts
@@ -1,8 +1,15 @@
 export const useClientsite = async () => {
-  const hostname = import.meta.client ? window.location.hostname.split('.')[0] : null
-
+  let hostname: string | undefined = ''
+  if (import.meta.client) {
+    hostname = window.location.hostname.split('.')[0]
+  } else {
+    const host = useNuxtApp().ssrContext?.event.node.req.headers.host
+    hostname = host ? host.split('.')[0] : ''
+  }
+  // hostname = 'GameDev'
   if (!hostname) return null
 
   const { data } = await useAsyncData(`clientsite-${hostname}`, () => $fetch(`/api/clients/slug/${hostname}`))
+
   return data
 }
