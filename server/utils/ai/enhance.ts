f757c5e84b33bb45d1dfb04f75ed8c9bff54eee5
germondai <bruno.krocek@gmail.com>
2025-10-10 18:05:05 +0200

    created enhance prompt ai util

---
+++ server/utils/ai/enhance.ts
@@ -0,0 +1,14 @@
+import { generateText } from 'ai'
+
+export const enhancePrompt = async (prompt: string) => {
+  const output = await generateText({
+    model: xai('grok-4-fast'),
+    system: `
+      You are a helpful assistant that enhances user prompts.
+      You improve the clarity, detail, and creativity of the prompts while keeping them concise.
+    `,
+    prompt: prompt.trim(),
+  })
+
+  return output
+}
