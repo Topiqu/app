dc8af4694f104adaa35326da45d1f114af69ba6b
Big John <vojtaben15@seznam.cz>
2025-10-11 15:21:33 +0200

    cleaner useClientsite

--- app/composables/useClientsite.ts
+++ app/composables/useClientsite.ts
@@ -1,14 +1,8 @@
 export const useClientSite = async () => {
-  let hostname: string | undefined = ''
-  if (import.meta.client) {
-    hostname = window.location.hostname.split('.')[0]
-  } else {
-    const host = useNuxtApp().ssrContext?.event.node.req.headers.host
-    hostname = host ? host.split('.')[0] : ''
-  }
-  // hostname = 'GameDev'
+  const hostname = import.meta.client
+    ? useBrowserLocation().value.hostname?.split('.')[0]
+    : (useNuxtApp().ssrContext?.event.node.req.headers.host?.split('.')[0] ?? '')
   if (!hostname) return null
-
   const { data } = await useAsyncData(`clientsite-${hostname}`, () => $fetch(`/api/clients/slug/${hostname}`))
 
   return data.value
