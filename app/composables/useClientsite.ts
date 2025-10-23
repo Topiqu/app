f5125e34c58dac05c18394651b49b58b99823c3d
Big John <vojtaben15@seznam.cz>
2025-10-23 12:07:30 +0200

    small clean up for useclientsite

--- app/composables/useClientsite.ts
+++ app/composables/useClientsite.ts
@@ -5,13 +5,12 @@ declare global {
 export const useClientSite = async () => {
   if (import.meta.client && globalThis.gtagInit === undefined) globalThis.gtagInit = false
 
-  const hostname = import.meta.client
-    ? useBrowserLocation().value.hostname?.replace('www.', '').split(':')[0]
-    : (useNuxtApp().ssrContext?.event.node.req.headers.host?.replace('www.', '').split(':')[0] ?? '')
+  const raw = useRequestURL().hostname ?? ''
 
-  if (!hostname) return
+  const hostname = raw?.split(':')[0]?.replace(/^www\./, '')
 
   const { data } = await useAsyncData(`clientsite-${hostname}`, () => $fetch(`/api/clients/slug/${hostname}`))
+
   const gtagId = data.value?.gtagId
 
   if (import.meta.client && data.value?.plan !== 'BASIC' && gtagId && !globalThis.gtagInit) {
