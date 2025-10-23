1284f58e99d5a2005871515e15bfffea401bf7aa
Big John <vojtaben15@seznam.cz>
2025-10-23 10:52:53 +0200

    fixed loading error caused by undefined articles prop

--- app/components/Article/Related.vue
+++ app/components/Article/Related.vue
@@ -1,5 +1,5 @@
 <template>
-  <section v-if="articles.length" class="mt-20">
+  <section v-if="articles && articles.length" class="mt-20">
     <h2 class="text-2xl font-bold mb-8 tracking-tight">{{ $t('articles.relatedArticles.title') }}</h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       <ArticleSkeletonCard
