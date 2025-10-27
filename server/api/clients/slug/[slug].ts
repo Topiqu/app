a8253471e66dd9f2334467427f393d508d821120
Big John <vojtaben15@seznam.cz>
2025-10-27 19:31:10 +0100

    testing slug fetching

--- server/api/clients/slug/[slug].ts
+++ server/api/clients/slug/[slug].ts
@@ -1,13 +1,13 @@
 export default defineEventHandler(async (event) => {
   const slug = getRouterParam(event, 'slug')
   if (!slug) throw createError({ statusCode: 400, message: 'Neplatný požadavek' })
-
-  const clientSite = await prisma.clientSite.findFirst({
-    where: { name: slug },
-  })
+  console.log(slug)
   // const clientSite = await prisma.clientSite.findFirst({
-  //   where: { subdomain: slug },
-  // }) // FOR PRODUCTION
+  //   where: { name: slug },
+  // })
+  const clientSite = await prisma.clientSite.findFirst({
+    where: { subdomain: slug },
+  }) // FOR PRODUCTION
 
   if (!clientSite) throw createError({ statusCode: 404, message: 'Blog nenalezen' })
 
