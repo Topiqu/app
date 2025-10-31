f40366a212598c7beefd09b6a96289207e5ca07f
Big John <vojtaben15@seznam.cz>
2025-10-31 21:07:11 +0100

    draft post now also accepts imageurl

--- server/api/articles/draft.post.ts
+++ server/api/articles/draft.post.ts
@@ -2,7 +2,8 @@ export default defineEventHandler(async (event) => {
   const user = (await getServerSession(event))?.user
   if (!user) throw createError({ statusCode: 401, message: 'Neautorizováno' })
 
-  const { title, excerpt, content } = await readBody(event)
+  const { title, excerpt, content, imageUrl } = await readBody(event)
+
   if (!title && !content && content !== '<p></p>' && !excerpt)
     throw createError({ statusCode: 400, message: 'Alespoň jedno pole (title, excerpt, content) musí být vyplněno' })
   if (!user.clientSiteId) throw createError({ statusCode: 400, message: 'Chybí clientSiteId' })
@@ -16,10 +17,19 @@ export default defineEventHandler(async (event) => {
       title: title || '',
       excerpt: excerpt || null,
       content: content || '',
+      imageUrl: imageUrl || null,
       userId: user.id,
       clientSiteId: user.clientSiteId,
     },
-    select: { id: true, title: true, excerpt: true, content: true, createdAt: true, updatedAt: true },
+    select: {
+      id: true,
+      title: true,
+      excerpt: true,
+      content: true,
+      imageUrl: true,
+      createdAt: true,
+      updatedAt: true,
+    },
   })
 
   return { success: true, draft }
