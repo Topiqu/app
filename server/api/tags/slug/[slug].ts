5ec37772b15feb8613436565807ef66741f1ff79
Big John <vojtaben15@seznam.cz>
2025-11-05 13:23:17 +0100

    reworked tags page

--- server/api/tags/slug/[slug].ts
+++ server/api/tags/slug/[slug].ts
@@ -1,11 +1,40 @@
 export default defineEventHandler(async (event) => {
-  const slug = getRouterParam(event, 'slug')?.toLowerCase()
-  if (!slug) throw createError({ statusCode: 400, message: 'Slug je povinný' })
+  const user = (await getServerSession(event))?.user
+  const tagName = decodeURIComponent(getRouterParam(event, 'slug')!.trim())
+  if (!tagName) throw createError({ statusCode: 400, message: 'Neplatný požadavek' })
 
-  const tag = await prisma.tag.findFirst({
-    where: { slug },
+  const { skip, take } = await getPagination(event)
+  const query = getQuery(event)
+  const { search = '', sort = 'createdAt:desc', site } = query as { search?: string; sort?: string; site: string }
+  if (!site) throw createError({ statusCode: 400, message: 'Chybí název blogu' })
+
+  const db = await getEnhancedPrisma(user)
+
+  const clientSite = await db.clientSite.findUnique({
+    where: { name: site },
+    select: { id: true },
+  })
+  if (!clientSite) throw createError({ statusCode: 404, message: 'Blog nenalezen' })
+
+  const [field, order] = (sort as string).split(':') as ['createdAt' | 'title', 'asc' | 'desc']
+
+  const tag = await db.tag.findFirst({
+    where: {
+      slug: tagName,
+      clientSiteId: clientSite.id,
+    },
     include: {
       articles: {
+        where: search
+          ? {
+              article: {
+                title: { contains: search, mode: 'insensitive' },
+              },
+            }
+          : undefined,
+        take: take + 1,
+        skip,
+        orderBy: { article: { [field]: order } },
         include: {
           article: {
             include: {
@@ -21,14 +50,18 @@ export default defineEventHandler(async (event) => {
 
   if (!tag) throw createError({ statusCode: 404, message: 'Tag nenalezen' })
 
+  const hasMore = tag.articles.length > take
+  const items = hasMore ? tag.articles.slice(0, take) : tag.articles
+
   return {
     ...tag,
-    articles: tag.articles.map((a) => ({
+    articles: items.map((a) => ({
       ...a,
       article: {
         ...a.article,
         likes: a.article._count.reactions,
       },
     })),
+    hasMore,
   }
 })
