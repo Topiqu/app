05679ccad170749740abf9abcd2aa22146d428b9
Big John <vojtaben15@seznam.cz>
2025-11-09 10:54:50 +0100

    returned old version of i18n

--- server/utils/i18n.ts
+++ server/utils/i18n.ts
@@ -1,37 +1,22 @@
 import type { H3Event } from 'h3'
 
-import { resolve } from 'node:path'
-import { readFile } from 'node:fs/promises'
-
 type Locale = 'cs' | 'en' | ({} & string)
 
 const messageCache = new Map<Locale, any>()
-type Messages = any
 
-const localesDir = resolve(process.cwd(), 'i18n/locales')
+type Messages = ReturnType<(typeof messageCache)['get']>
 
 export const getServerLocaleMessages = async (locale: Locale): Promise<Messages> => {
-  if (messageCache.has(locale)) return messageCache.get(locale)
+  if (!messageCache.has(locale))
+    try {
+      const messages = await import(`~~/i18n/locales/${locale}.json`)
+      messageCache.set(locale, messages.default)
+    } catch {
+      const fallback = await import('~~/i18n/locales/en.json')
+      messageCache.set(locale, fallback.default)
+    }
 
-  const filePath = resolve(localesDir, `${locale.toLowerCase()}.json`)
-  let raw: string
-
-  try {
-    raw = await readFile(filePath, 'utf-8')
-  } catch {
-    raw = await readFile(resolve(localesDir, 'en.json'), 'utf-8')
-  }
-
-  let messages
-  try {
-    messages = JSON.parse(raw)
-  } catch {
-    const enRaw = await readFile(resolve(localesDir, 'en.json'), 'utf-8')
-    messages = JSON.parse(enRaw)
-  }
-
-  messageCache.set(locale, messages)
-  return messages
+  return messageCache.get(locale)
 }
 
 export const useServerI18n = async (
@@ -42,12 +27,16 @@ export const useServerI18n = async (
   } = {},
 ) => {
   const locale: Locale = opts.locale || getCookie(event, 'i18n_lang') || 'en'
-  const messages: Messages = opts.messages || (await getServerLocaleMessages(locale))
+  const messages: Messages = opts.messages || (await getServerLocaleMessages(locale)) || {}
 
   const translate = (key: string, params?: Record<string, any>) => {
-    const template = key.split('.').reduce((o, k) => o?.[k], messages) as string
-    if (!template) return key
-    return params ? template.replace(/{(\w+)}/g, (_, k) => params[k]?.toString() ?? _) : template
+    const template = key.split('.').reduce((obj, k) => obj?.[k], messages) as string
+
+    if (!template) return console.warn(`Translation key "${key}" not found for locale "${locale}"`)
+
+    if (params) return template.replace(/{(\w+)}/g, (match, paramKey) => params[paramKey]?.toString() || match)
+
+    return template
   }
 
   return { translate }
