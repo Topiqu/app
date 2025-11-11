c0c0965828bda91b0e9b63590bfb9b8185e5c464
germondai <bruno.krocek@gmail.com>
2025-11-11 13:06:57 +0100

    follows notifications localisation

--- server/api/follows/index.post.ts
+++ server/api/follows/index.post.ts
@@ -1,47 +1,27 @@
 export default defineEventHandler(async (event) => {
+  const { translate: t } = await useServerI18n(event)
+
   const user = (await getServerSession(event))?.user
-  if (!user) throw createError({ statusCode: 401, message: 'Neautorizováno' })
+  if (!user) throw createError({ statusCode: 401, message: t('common.errors.unauthorized')! })
 
   const body = await readValidatedBody(event, z.object({ followedId: z.string() }).parse)
+  if (user.id === body.followedId)
+    throw createError({ statusCode: 400, message: t('common.errors.cannotFollowYourself')! })
 
   const db = await getEnhancedPrisma(user)
-  const followedUser = await db.user.findFirst({
-    where: { id: body.followedId },
-  })
 
-  if (!followedUser)
-    throw createError({
-      statusCode: 404,
-      message: 'Uživatel nenalezen.',
-    })
+  const followedUser = await db.user.findFirst({ where: { id: body.followedId } })
+  if (!followedUser) throw createError({ statusCode: 404, message: t('common.errors.userNotFound')! })
 
-  if (user.id === body.followedId)
-    throw createError({
-      statusCode: 400,
-      message: 'Nemůžete sledovat sám sebe.',
-    })
+  const existingFollow = await db.follow.findFirst({ where: { followerId: user.id, followedId: body.followedId } })
+  if (existingFollow) throw createError({ statusCode: 409, message: t('common.errors.alreadyFollowingUser')! })
 
-  const existingFollow = await db.follow.findFirst({
-    where: { followerId: user.id, followedId: body.followedId },
-  })
-
-  if (existingFollow)
-    throw createError({
-      statusCode: 409,
-      message: 'Už tohoto uživatele sledujete.',
-    })
-
-  const follow = await db.follow.create({
-    data: {
-      followerId: user.id,
-      followedId: body.followedId,
-    },
-  })
+  const follow = await db.follow.create({ data: { followerId: user.id, followedId: body.followedId } })
 
   await prisma.notification.create({
     data: {
       userId: body.followedId,
-      message: `Uživatel ${user.name} vás začal sledovat!`,
+      message: t('notifications.newFollower', { user: user.name || 'Anonymous' })!,
       type: 'FOLLOW',
     },
   })
